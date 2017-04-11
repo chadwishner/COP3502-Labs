0c1b661850bf3c979d239749b18be9f487f5fc8c
chadwishner <chadwishner@gmail.com>
2017-04-11 16:06:30 -0400

    Add files via upload

---
+++ Lab13.java
@@ -0,0 +1,13 @@
+public class Lab13 {
+	public static void manin(String [] args){
+		
+		Shape x = new Circle("purple", 5);
+		System.out.println(x);
+		
+		x = new Rectangle("gray", 8, 11);
+		System.out.println(x);
+		
+		x = new Triangle("orange", 5, 10);
+		System.out.println(x);
+	}
+}
